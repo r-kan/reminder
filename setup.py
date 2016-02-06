d9f6c84fe2659bc29b6b36438401a86a4592d765
r-kan <its_right@msn.com>
2016-02-07 01:49:15 +0800

    add setup.py for resolving library dependency

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from setuptools import setup
+
+setup(
+    name='reminder',
+    version='0.1',
+    description='better reminder, more customizable slideshow tool',
+    url='https://github.com/r-kan/reminder',
+    author='rkan',
+    author_email='its_right@msn.com',
+    install_requires=[
+        'pillow',
+        'requests',
+    ],
+)
